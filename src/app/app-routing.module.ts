25d26a531220e579cf27575177bca220c4b96174
Ashraful Gafur <ashraful.gafur@streamstech.com>
2022-12-10 04:11:14 +0600

    Before Refactor

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -44,7 +44,6 @@ const routes: Routes = [
       {path:"button", component:ButtonsComponent},
     ]
   },
-
   {path:"", redirectTo:"/home", pathMatch:"full"},
   {path:"**", redirectTo:"/home", pathMatch:"full"},
 ];
