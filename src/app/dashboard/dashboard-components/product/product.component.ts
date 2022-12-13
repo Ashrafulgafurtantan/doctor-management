12dee32a06413dcfdc0296fcde44174da9b18636
Ashraful Gafur <ashraful.gafur@streamstech.com>
2022-12-14 04:14:42 +0600

    Create Attendance

--- src/app/dashboard/dashboard-components/product/product.component.ts
+++ src/app/dashboard/dashboard-components/product/product.component.ts
@@ -1,20 +1,21 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit, ViewChild} from '@angular/core';
+import {MatSort} from "@angular/material/sort";
+import {MatPaginator} from "@angular/material/paginator";
+import {MatTableDataSource} from "@angular/material/table";
 
 export interface PeriodicElement {
   id: number;
   name: string;
-  work: string;
-  project: string;
   priority: string;
   badge: string;
-  budget: string;
+  date: string;
 }
 
 const ELEMENT_DATA: PeriodicElement[] = [
-  { id: 1, name: 'Deep Javiya', work: 'Frontend Devloper', project: 'Flexy Angular', priority: 'Low', badge: 'badge-info', budget: '$3.9k' },
-  { id: 2, name: 'Nirav Joshi', work: 'Project Manager', project: 'Hosting Press HTML', priority: 'Medium', badge: 'badge-primary', budget: '$24.5k' },
-  { id: 3, name: 'Sunil Joshi', work: 'Web Designer', project: 'Elite Admin', priority: 'High', badge: 'badge-danger', budget: '$12.8k' },
-  { id: 4, name: 'Maruti Makwana', work: 'Backend Devloper', project: 'Material Pro', priority: 'Critical', badge: 'badge-success', budget: '$2.4k' },
+  { id: 1, name: 'Deep Javiya',  priority: 'Present', badge: 'badge-success', date: '2027/11/08', },
+  { id: 2, name: 'Nirav Joshi',   priority: 'Present', badge: 'badge-success', date: '2019/02/1', },
+  { id: 3, name: 'Sunil Joshi',  priority: 'Absent', badge: 'badge-danger', date: '2021/9/23', },
+  { id: 4, name: 'Maruti Makwana',  priority: 'Absent', badge: 'badge-danger', date: '2016/08/5', },
 ];
 
 
@@ -25,12 +26,23 @@ const ELEMENT_DATA: PeriodicElement[] = [
 })
 export class ProductComponent implements OnInit {
 
-  displayedColumns: string[] = ['id', 'assigned', 'name', 'priority', 'budget'];
-  dataSource = ELEMENT_DATA;
+  displayedColumns: string[] = ['id', 'assigned', 'priority', 'date'];
+  dataSource:MatTableDataSource<any>;
+  products:PeriodicElement[];
 
-  constructor() { }
-
-  ngOnInit(): void {
+  @ViewChild(MatSort,{static:true}) sort: MatSort | any;
+  @ViewChild(MatPaginator,{static:true}) paginator: MatPaginator | any;
+  constructor() {
+    this.products=ELEMENT_DATA;
+    this.dataSource = new MatTableDataSource(this.products);
   }
 
+  ngOnInit(): void {
+    this.dataSource.sort = this.sort;
+    this.dataSource.paginator = this.paginator;
+  }
+  applyFilter(e: any):void{
+    const filterValue = e.value;
+    this.dataSource.filter = filterValue.trim().toLowerCase();
+  }
 }
